45604ee7a180e053921a81d060c7787c85e42b7e
Lee Jisu <itis821@ajou.ac.kr>
2023-11-21 18:11:37 +0900

    [STYLE] 버튼 MUI 로 변경

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './imgs/logo.png';
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <img src={logo} id = 'logo' alt='로고 이미지'/>
       <p>댓글창</p>
-      <button>입력</button>
+      <Button variant="contained">등록</Button>
       <p>댓글 리스트</p>
     </div>
   );
