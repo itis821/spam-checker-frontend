c27aa3a20605928b2924c98a343ffcfbd8165bbe
Lee Jisu <itis821@ajou.ac.kr>
2023-11-21 17:50:50 +0900

    [INIT] 프로젝트 생성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import logo from './imgs/logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} id = 'logo' alt='로고 이미지'/>
+      <p>댓글창</p>
+      <button>입력</button>
+      <p>댓글 리스트</p>
     </div>
   );
 }
