65b840ae9e46144a9e73a4ab4282624a3ed388f7
Lee Jisu <itis821@ajou.ac.kr>
2023-11-21 18:21:23 +0900

    [STYLE] 댓글입력 MUI 적용, 스타일 적용

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import logo from './imgs/logo.png';
 import './App.css';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 function App() {
   return (
     <div className="App">
+
       <img src={logo} id = 'logo' alt='로고 이미지'/>
-      <p>댓글창</p>
-      <Button variant="contained">등록</Button>
-      <p>댓글 리스트</p>
+
+      <div className='comment'>
+        <TextField id="cmt" label="댓글 입력" variant="standard" />
+        <Button id= "cmt_btn" variant="contained">등록</Button>
+      </div>
+      
+      <div className='comment_list'>
+        <p>댓글 리스트</p>
+      </div>
+      
     </div>
   );
 }
