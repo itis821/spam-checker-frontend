9704f62089f9b6d0d3c80ca7a4e5056fc7d69291
Lee Jisu <itis821@ajou.ac.kr>
2023-12-06 19:16:44 +0900

    [FEAT] 시간 표시  변경 코드 추가

--- src/component/CommentList.js
+++ src/component/CommentList.js
@@ -5,11 +5,17 @@ const CommentList = ({commentList}) => {
                 <div key = {comment.id}>
                     <div>익명{comment.id}</div>
                     <div>{comment.contents}</div>
-                    <div>{comment.createdDate}</div>
+                    <div>{formatDate(comment.createdDate)}</div>
                 </div>
             ))}
         </div>
     )
 };
+
+function formatDate(dateString) {
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
+    const date = new Date(dateString);
+    return date.toLocaleString('ko-KR', options);
+}
 
 export default CommentList;
